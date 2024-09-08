1b0102ba057dd5b1b97a2d28e9b96bbf7423327e
Thierry Champion <thierry.champion@polymtl.ca>
2024-09-07 23:52:34 -0400

    feat(page): simple create page

--- client/src/app/pages/create-page/create-page.component.ts
+++ client/src/app/pages/create-page/create-page.component.ts
@@ -9,5 +9,35 @@ import { RouterLink } from '@angular/router';
     imports: [RouterLink],
 })
 export class CreatePageComponent {
-    constructor() {}
+    selectedMap: string = ''; // TODO change to the map type
+    mapList: Array<string>;
+    loading: boolean = true;
+
+    constructor() {
+        // TODO fetch the map list from the server.
+        // Only display the visible ones.
+        this.mapList = [];
+    }
+
+    onSelectMap(event: Event): void {
+        const mapElement = event.target as HTMLElement;
+
+        if (mapElement.tagName.toLowerCase() === 'span') {
+            this.selectedMap = mapElement.innerText;
+            this.updateDescription();
+            this.updatePreview();
+        }
+    }
+
+    updateDescription(): void {
+        // TODO
+    }
+
+    updatePreview(): void {
+        // TODO
+    }
+
+    isSelectionValid(): boolean {
+        return this.selectedMap !== '';
+    }
 }
