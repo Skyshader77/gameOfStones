99d6d21e0f8d5d85fc5631414894ba576f02bd45
Coossy <charles-emilek7@hotmail.com>
2024-09-16 12:59:36 -0400

    feat(form):player creation modal completed

--- client/src/app/pages/player-creation-page/player-creation-page.component.ts
+++ client/src/app/pages/player-creation-page/player-creation-page.component.ts
@@ -1,30 +1,70 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircleInfo, faDiceFour, faDiceSix, faShieldHalved } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-player-creation-page',
     standalone: true,
     templateUrl: './player-creation-page.component.html',
-    styleUrls: [],
-    imports: [RouterLink, ReactiveFormsModule],
+    styleUrls: ['./player-creation-page.component.scss'],
+    imports: [RouterLink, ReactiveFormsModule, FontAwesomeModule, CommonModule],
 })
-export class PlayerCreationPageComponent {
-    // TODO how to do the avatars (carousel, card dropdown, menu, etc.)?
-    // TODO how to restrain options to what we want?
-    // TODO how to do dynamic updating of the available avatars with the lobby?
+export class PlayerCreationPageComponent implements OnInit {
+    faCircleInfo = faCircleInfo;
+    faDiceSix = faDiceSix;
+    faDiceFour = faDiceFour;
+    faShieldHalved = faShieldHalved;
 
-    playerForm = new FormGroup({
-        name: new FormControl('', Validators.required),
-        avatar: new FormControl('', Validators.required),
-        bonus: new FormControl('', Validators.required),
-        d6Bonus: new FormControl('', Validators.required),
-    });
+    playerForm: FormGroup;
+    selectedAvatar: string = '../../../assets/goat.jpg';
 
-    constructor(private router: Router) {}
+    selectedBonus: string | null = null;
+    lifeHearts: boolean[] = [true, true, true, true, false, false];
+    speedTriangles: boolean[] = [true, true, true, true, false, false];
+    attackStars: boolean[] = [true, true, true, true];
+    defenseHexagons: boolean[] = [true, true, true, true];
+
+    constructor(
+        private fb: FormBuilder,
+        private router: Router,
+    ) {
+        this.playerForm = this.fb.group({
+            name: [''],
+            bonus: [''],
+            d6Bonus: [],
+        });
+    }
+
+    changeAvatar(avatar: string): void {
+        this.selectedAvatar = avatar;
+        this.playerForm.get('avatar')?.setValue(avatar);
+    }
+
+    updateBonus(selectedBonus: string): void {
+        if (selectedBonus === 'life') {
+            this.lifeHearts = [true, true, true, true, true, true];
+            this.speedTriangles = [true, true, true, true, false, false];
+        } else if (selectedBonus === 'speed') {
+            this.speedTriangles = [true, true, true, true, true, true];
+            this.lifeHearts = [true, true, true, true, false, false];
+        }
+    }
+
+    onBonusChange(bonus: string): void {
+        this.selectedBonus = bonus;
+    }
+
+    ngOnInit(): void {
+        // Initialisation si nécessaire
+    }
 
     onSubmit() {
-        // TODO send post to server or via websocket
-        this.router.navigateByUrl('/lobby');
+        if (this.playerForm.valid) {
+            // Envoyer les données au serveur ou via websocket
+            this.router.navigateByUrl('/lobby');
+        }
     }
 }
