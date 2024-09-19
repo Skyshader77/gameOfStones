a719897a1ab6ef01907cb618a2249c9a87a30f69
Thierry Champion <thierry.champion@polymtl.ca>
2024-09-19 11:53:52 -0400

    fix(): error messages in french

--- client/src/app/services/lobby-creation.service.ts
+++ client/src/app/services/lobby-creation.service.ts
@@ -10,21 +10,31 @@ import { MapSelectionService } from './map-selection.service';
 export class LobbyCreationService {
     private mapAPIService: MapAPIService = inject(MapAPIService);
     private mapSelectionService: MapSelectionService = inject(MapSelectionService);
+    private _selectionError: string;
+
+    get selectionError(): string {
+        return this._selectionError;
+    }
 
     initialize(): void {
         this.mapSelectionService.initialize();
+        this._selectionError = '';
     }
 
     isSelectionValid(): Observable<boolean> {
         const selectedMap: Map | null = this.mapSelectionService.selectedMap;
 
-        if (!selectedMap) return of(false);
+        if (!selectedMap) {
+            this._selectionError = 'Aucune carte a été sélectionnée!';
+            return of(false);
+        }
 
         return this.mapAPIService.getMapbyId(selectedMap._id).pipe(
             map((serverMap: Map) => {
                 return serverMap._id === selectedMap._id; // TODO && map.isVisible;
             }),
             catchError(() => {
+                this._selectionError = "La carte sélectionnée n'existe plus!";
                 return of(false);
             }),
         );
