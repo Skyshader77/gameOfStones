9d7e9fd01de6c366e4b331f8ab1482f5bb4474be
Coossy <charles-emilek7@hotmail.com>
2024-09-22 20:21:48 -0400

    feat(name input): Adding a Name Validator

--- client/src/app/components/player-creation/player-creation.component.ts
+++ client/src/app/components/player-creation/player-creation.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AvatarListComponent } from '@app/components/avatar-list/avatar-list.component';
 import { HpSpeedSelectorComponent } from '@app/components/hp-speed-selector/hp-speed-selector.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -28,16 +28,24 @@ export class PlayerCreationComponent {
     placeHolder: number[];
     playerForm: FormGroup;
 
+    nameError: string | null = null;
+
     //   TODO for integration with dev:
     //   - change placeholder to something that is actually meaningful
     //   - make a constant for the default stats
     //   - make components for various parts of the form (avatar, stats)
     constructor() {
         this.playerForm = new FormGroup({
-            name: new FormControl('', Validators.required),
+            name: new FormControl('', [Validators.required, this.validateName()]),
             avatarId: new FormControl(0, Validators.required),
             statsBonus: new FormControl('', Validators.required),
             dice6: new FormControl('', Validators.required),
+        });
+        this.playerForm.get('name')?.valueChanges.subscribe(() => {
+            const control = this.playerForm.get('name');
+            this.nameError = control?.hasError('invalidName')
+                ? 'Le nom doit contenir entre 3 et 25 caractères, sans espaces ni caractères spéciaux.'
+                : null;
         });
         this.avatars = [
             'assets/avatar/goat.jpg',
@@ -63,5 +71,13 @@ export class PlayerCreationComponent {
 
     onSubmit(): void {
         this.submissionEvent.emit();
+    }
+
+    validateName(): ValidatorFn {
+        return (control: AbstractControl): { [key: string]: any } | null => {
+            const namePattern = /^[a-zA-Z0-9À-ÿ]{3,25}$/;
+            const nameValidated = namePattern.test(control.value);
+            return nameValidated ? null : { invalidName: { value: control.value } };
+        };
     }
 }
