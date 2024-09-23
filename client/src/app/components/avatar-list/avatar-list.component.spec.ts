5b859912236402a019fd22b1ac17c08c4d2f1245
Thierry Champion <thierry.champion@polymtl.ca>
2024-09-22 21:48:46 -0400

    fix: passes linter

--- client/src/app/components/avatar-list/avatar-list.component.spec.ts
+++ client/src/app/components/avatar-list/avatar-list.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AvatarListComponent } from './avatar-list.component';
 
 describe('AvatarListComponent', () => {
-  let component: AvatarListComponent;
-  let fixture: ComponentFixture<AvatarListComponent>;
+    let component: AvatarListComponent;
+    let fixture: ComponentFixture<AvatarListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AvatarListComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [AvatarListComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(AvatarListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(AvatarListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- client/src/app/components/stats-selector/stats-selector.component.spec.ts
+++ client/src/app/components/stats-selector/stats-selector.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StatsSelectorComponent } from './stats-selector.component';
 
 describe('StatsSelectorComponent', () => {
-  let component: StatsSelectorComponent;
-  let fixture: ComponentFixture<StatsSelectorComponent>;
+    let component: StatsSelectorComponent;
+    let fixture: ComponentFixture<StatsSelectorComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [StatsSelectorComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [StatsSelectorComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(StatsSelectorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(StatsSelectorComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
