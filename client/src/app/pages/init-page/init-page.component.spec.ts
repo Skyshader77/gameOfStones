2d92103e195671e55e4d5b00fb736eb971a9b049
Coossy <charles-emilek7@hotmail.com>
2024-09-30 18:21:22 -0400

    test(init-page): Improved Initial View Testing

--- client/src/app/pages/init-page/init-page.component.spec.ts
+++ client/src/app/pages/init-page/init-page.component.spec.ts
@@ -1,23 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { provideRouter, Route } from '@angular/router';
+import { provideRouter, Router, RouterLink, Routes } from '@angular/router';
 import { InitPageComponent } from './init-page.component';
-
-const routes: Route[] = [];
 
 describe('InitPageComponent', () => {
     let component: InitPageComponent;
     let fixture: ComponentFixture<InitPageComponent>;
+    let router: Router;
+    const routes: Routes = [];
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [InitPageComponent],
+            imports: [InitPageComponent, RouterLink],
             providers: [provideRouter(routes)],
         }).compileComponents();
 
         fixture = TestBed.createComponent(InitPageComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
+        router = TestBed.inject(Router);
     });
 
     it('should create', () => {
@@ -25,14 +26,22 @@ describe('InitPageComponent', () => {
     });
 
     it('should have correct routerLink for the "Créer" button', () => {
+        spyOn(router, 'navigateByUrl');
         const button = fixture.debugElement.query(By.css('button[routerLink="/create"]'));
         expect(button).toBeTruthy();
-        expect(button.attributes['ng-reflect-router-link']).toBe('/create');
+        const mockClick = new MouseEvent('click');
+        button.nativeElement.dispatchEvent(mockClick);
+        fixture.detectChanges();
+        expect(router.navigateByUrl).toHaveBeenCalledWith(router.createUrlTree(['/create']), jasmine.anything());
     });
 
     it('should have correct routerLink for the "Administrer" button', () => {
+        spyOn(router, 'navigateByUrl');
         const button = fixture.debugElement.query(By.css('button[routerLink="/admin"]'));
         expect(button).toBeTruthy();
-        expect(button.attributes['ng-reflect-router-link']).toBe('/admin');
+        const mockClick = new MouseEvent('click');
+        button.nativeElement.dispatchEvent(mockClick);
+        fixture.detectChanges();
+        expect(router.navigateByUrl).toHaveBeenCalledWith(router.createUrlTree(['/admin']), jasmine.anything());
     });
 });
