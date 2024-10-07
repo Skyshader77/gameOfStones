7aa568fc202e2ac031798559737c8132b5a481e9
NikolaiGTR <nikolai.olekhnovitch@polymtl.ca>
2024-10-07 01:47:42 -0400

    (hotfix) spec.ts file was updated with renamed chat component

--- client/src/app/components/chat/chat/chat.component.spec.ts
+++ client/src/app/components/chat/chat/chat.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GameChatComponent } from './chat.component';
+import { ChatComponent } from './chat.component';
 
 describe('GameChatComponent', () => {
-    let component: GameChatComponent;
-    let fixture: ComponentFixture<GameChatComponent>;
+    let component: ChatComponent;
+    let fixture: ComponentFixture<ChatComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [GameChatComponent],
+            imports: [ChatComponent],
         }).compileComponents();
 
-        fixture = TestBed.createComponent(GameChatComponent);
+        fixture = TestBed.createComponent(ChatComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
