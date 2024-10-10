0fd63fdf52162e787bdca16969ce8163988af862
Thierry Champion <thierry.champion@polymtl.ca>
2024-10-10 16:02:52 -0400

    feat: client socket interface and example server socket gateway

--- client/src/app/services/communication-services/socket.service.spec.ts
+++ client/src/app/services/communication-services/socket.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
-
 import { SocketService } from './socket.service';
+
+
 
 describe('SocketService', () => {
     let service: SocketService;
--- client/src/app/services/communication-services/socket.service.ts
+++ client/src/app/services/communication-services/socket.service.ts
@@ -1,31 +1,54 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { io, Socket } from 'socket.io-client';
 import { environment } from 'src/environments/environment';
+import { SocketRole } from '@app/constants/socket.constants';
 
 @Injectable({
     providedIn: 'root',
 })
 export class SocketService {
-    private socket: Socket;
+    private sockets: Map<SocketRole, Socket>;
 
     constructor() {
-        this.socket = io(environment.serverUrl, { transports: ['websocket'], upgrade: false });
+        this.sockets = new Map<SocketRole, Socket>();
+        this.connectSockets();
     }
 
-    disconnect() {
-        this.socket.disconnect();
+    disconnect(socketRole: SocketRole) {
+        this.sockets.get(socketRole)?.disconnect();
     }
 
-    on<T>(event: string): Observable<T> {
+    on<T>(socketRole: SocketRole, event: string): Observable<T> {
+        const socket = this.sockets.get(socketRole);
+        if (!socket) {
+            return throwError(() => new Error("Le socket demandé n'existe pas!"));
+        }
         return new Observable<T>((subscriber) => {
-            this.socket.on(event, (data: T) => {
+            socket.on(event, (data: T) => {
                 subscriber.next(data);
             });
         });
     }
 
-    send<T>(event: string, data?: T): void {
-        this.socket.emit(event, data);
+    emit<T>(socketRole: SocketRole, event: string, data?: T): void {
+        const socket = this.sockets.get(socketRole);
+        if (!socket) {
+            throwError(() => new Error("Le socket demandé n'existe pas!"));
+        } else {
+            socket.emit(event, data);
+        }
+    }
+
+    joinRoom(roomId: string): void {
+        for (const socket of this.sockets) {
+            socket[1].emit('joinRoom', roomId); // TODO a redefinir quelque part
+        }
+    }
+
+    private connectSockets() {
+        for (const role of Object.values(SocketRole)) {
+            this.sockets.set(role, io(environment.serverUrl + role, { transports: ['websocket'], upgrade: false }));
+        }
     }
 }
