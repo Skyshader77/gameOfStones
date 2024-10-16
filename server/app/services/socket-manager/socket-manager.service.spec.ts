032cb9d8e573a07ccfa8407c04726c2ea44e7797
Thierry Champion <thierry.champion@polymtl.ca>
2024-10-16 13:14:49 -0400

    fix: passes tests

--- server/app/services/socket-manager/socket-manager.service.spec.ts
+++ server/app/services/socket-manager/socket-manager.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SocketManagerService } from './socket-manager.service';
+import { RoomManagerService } from '@app/services/room-manager/room-manager.service';
+
+const roomManagerMock = {
+    getRoom: jest.fn().mockReturnValue('ABCD'),
+};
 
 describe('SocketManagerService', () => {
     let service: SocketManagerService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [SocketManagerService],
+            providers: [SocketManagerService, { provide: RoomManagerService, useValue: roomManagerMock }],
         }).compile();
 
         service = module.get<SocketManagerService>(SocketManagerService);
