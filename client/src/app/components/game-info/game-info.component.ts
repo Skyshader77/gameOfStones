67dcd56583245b7e6f2648f8ed3111f8150422a0
Coossy <charles-emilek7@hotmail.com>
2024-10-19 19:20:35 -0400

    feat(game-info): skeleton acces to information

--- client/src/app/components/game-info/game-info.component.ts
+++ client/src/app/components/game-info/game-info.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCrown, faPerson, faRobot } from '@fortawesome/free-solid-svg-icons';
+export interface PlayerField {
+    name: string;
+}
+export interface MapField {
+    size: string;
+}
+export interface GameField {
+    numberPlayer: number;
+}
 
 @Component({
     selector: 'app-game-info',
@@ -13,4 +22,16 @@ export class GameInfoComponent {
     robotIcon = faRobot;
     humanIcon = faPerson;
     crownIcon = faCrown;
+
+    mapField: MapField = {
+        size: '20 x 20',
+    };
+
+    playerField: PlayerField = {
+        name: 'John Doe',
+    };
+
+    gameField: GameField = {
+        numberPlayer: 6,
+    };
 }
