f422383e0bdf858e12e96a23a1017142a29717c1
Skyshader77 <anguyen2050813@gmail.com>
2024-10-22 09:22:32 -0400

    removed filter for cleanup

--- server/app/index.ts
+++ server/app/index.ts
@@ -3,14 +3,13 @@ import { AppModule } from '@app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AllExceptionsFilter } from './common/filters/all-exceptions.filter'; // Adjust the path as needed
 
 const bootstrap = async () => {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
     app.useGlobalPipes(new ValidationPipe());
     app.enableCors({
-        origin: '*', // Adjust according to your requirements
+        origin: '*',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
         allowedHeaders: 'Content-Type, Authorization',
     });
@@ -22,10 +21,6 @@ const bootstrap = async () => {
         .build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api/docs', app, document);
-
-    // Register the global exception filter
-    app.useGlobalFilters(new AllExceptionsFilter());
-
 
     await app.listen(process.env.PORT);
 };
