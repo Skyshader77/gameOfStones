34b08f2f908556ebe725e01246f16597c5ad0310
Nabil Ibarissen <nabilibarissen2003@gmail.com>
2024-10-22 10:51:47 -0400

    feature: game logic socket communicator

---
+++ client/src/app/services/communication-services/game-logic-socket.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { GameEvents, SocketRole } from '@app/constants/socket.constants';
+import { MoveData } from '@app/interfaces/reachableTiles';
+import { SocketService } from './socket.service';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class GameLogicSocketService {
+    constructor(private socketService: SocketService) {}
+
+    processMovement(movementData: MoveData) {
+        this.socketService.getSockets.get(SocketRole.GAME)?.emit(GameEvents.DesiredMove, movementData);
+    }
+}
