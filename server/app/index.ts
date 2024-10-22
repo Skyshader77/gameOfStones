573f8243a74bfb112f234e7f459709739c5fb721
Alex Ng <anguyen2050813@gmail.com>
2024-10-22 02:46:43 +0000

    Revert "fix: forgot src in path to index"
    This reverts commit ac4de5c34b6322b710e3064e34ab05154521a74e

--- server/app/index.ts
+++ server/app/index.ts
@@ -30,7 +30,7 @@ const bootstrap = async () => {
         if (req.path.startsWith('/api')) {
             next();
         } else {
-            res.sendFile(join(__dirname, '..', 'client', 'src', 'index.html'));
+            res.sendFile(join(__dirname, '..', 'client', 'index.html'));
         }
     });
 
@@ -38,5 +38,4 @@ const bootstrap = async () => {
 };
 
 bootstrap();
-
 
