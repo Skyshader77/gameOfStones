e5fb5f6a0dc6fcf65cb8f6157156f6ccbfc239f5
Alex Ng <anguyen2050813@gmail.com>
2024-10-22 02:47:04 +0000

    Revert "fix: fix error 404 everytime we refresh"
    This reverts commit 629ea3a7e76422a53e276616feff097195a20b81

--- server/app/index.ts
+++ server/app/index.ts
@@ -2,8 +2,7 @@ import { AppModule } from '@app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as express from 'express';
-import { join } from 'path';
+
 const bootstrap = async () => {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
@@ -22,20 +21,7 @@ const bootstrap = async () => {
     SwaggerModule.setup('api/docs', app, document);
     SwaggerModule.setup('', app, document);
 
-    // Serve static files
-    app.use(express.static(join(__dirname, '..', 'client')));
-
-    // Handle client-side routing
-    app.use('*', (req, res, next) => {
-        if (req.path.startsWith('/api')) {
-            next();
-        } else {
-            res.sendFile(join(__dirname, '..', 'client', 'index.html'));
-        }
-    });
-
     await app.listen(process.env.PORT);
 };
 
 bootstrap();
-
