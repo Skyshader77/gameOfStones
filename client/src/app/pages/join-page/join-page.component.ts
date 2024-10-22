2ca014cfc25043f5e9cf1055ff0edd285a447b40
NikolaiGTR <nikolai.olekhnovitch@polymtl.ca>
2024-10-21 21:03:35 -0400

    (feat) Join room handles new players and adds them to the room via sockets

--- client/src/app/pages/join-page/join-page.component.ts
+++ client/src/app/pages/join-page/join-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { RoomJoiningService } from '@app/room-services/room-joining.service';
 import { FormsModule } from '@angular/forms';
 import { PlayerCreationComponent } from '@app/components/player-creation/player-creation.component';
@@ -8,24 +8,30 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ModalMessageService } from '@app/services/utilitary/modal-message.service';
 import { MessageDialogComponent } from '@app/components/message-dialog/message-dialog.component';
 import * as constants from '@app/constants/join-page.constants';
+import { PlayerCreationService } from '@app/services/player-creation-services/player-creation.service';
+import { PlayerRole } from '@common/interfaces/player.constants';
+import { Statistic } from '@app/interfaces/stats';
 
 @Component({
     selector: 'app-join-page',
     standalone: true,
     templateUrl: './join-page.component.html',
     styleUrls: [],
-    imports: [RouterLink, FontAwesomeModule, FormsModule, PlayerCreationComponent, MessageDialogComponent],
+    imports: [RouterLink, FontAwesomeModule, FormsModule, PlayerCreationComponent, MessageDialogComponent, PlayerCreationComponent],
 })
 export class JoinPageComponent {
     @ViewChild('playerCreationModal') playerCreationModal!: ElementRef<HTMLDialogElement>;
 
     userInput: string = '';
+    roomCode: string = '';
     inputPlaceholder: string = 'Entrez le code de la partie';
     formIcon = FORM_ICONS;
 
     constructor(
         private roomJoiningService: RoomJoiningService,
         private modalMessageService: ModalMessageService,
+        private playerCreationService: PlayerCreationService,
+        private routerService: Router,
     ) {}
 
     onJoinClicked(): void {
@@ -41,8 +47,15 @@ export class JoinPageComponent {
                     content: constants.JOIN_ERRORS.roomDoesNotExist,
                 });
             } else {
+                this.roomCode = this.userInput;
                 this.playerCreationModal.nativeElement.showModal();
             }
         });
+    }
+
+    onSubmit(formData: { name: string; avatarId: number; statsBonus: Statistic; dice6: Statistic }): void {
+        const newPlayer = this.playerCreationService.createPlayer(formData, PlayerRole.HUMAN);
+        this.roomJoiningService.joinRoom(this.roomCode, newPlayer);
+        this.routerService.navigate(['/room', this.roomCode]);
     }
 }
--- client/src/app/room-services/room-joining.service.ts
+++ client/src/app/room-services/room-joining.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RoomAPIService } from '@app/services/api-services/room-api.service';
+import { RoomSocketService } from '@app/services/communication-services/room-socket.service';
+import { Player } from '@app/interfaces/player';
 
 @Injectable({
     providedIn: 'root',
 })
 export class RoomJoiningService {
-    constructor(private roomAPIService: RoomAPIService) {}
+    constructor(
+        private roomAPIService: RoomAPIService,
+        private roomSocketService: RoomSocketService,
+    ) {}
 
     isValidInput(input: string): boolean {
         const regex = /^\d{4}$/;
@@ -15,5 +20,9 @@ export class RoomJoiningService {
 
     isIDValid(input: string): Observable<boolean> {
         return this.roomAPIService.checkRoomExists(input);
+    }
+
+    joinRoom(roomCode: string, player: Player) {
+        this.roomSocketService.joinRoom(roomCode, player);
     }
 }
