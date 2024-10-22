eb15d2b0a48161c697446fbcec1f9e4d18e3d7cd
Thierry Champion <thierry.champion@polymtl.ca>
2024-10-21 20:22:50 -0400

    fix: linter

--- client/src/app/services/utilitary/refresh.service.spec.ts
+++ client/src/app/services/utilitary/refresh.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { RefreshService } from './refresh.service';
 
 describe('RefreshService', () => {
-  let service: RefreshService;
+    let service: RefreshService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(RefreshService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(RefreshService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
