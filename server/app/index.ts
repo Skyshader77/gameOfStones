a89408a0530ca29a9164d41adffe3ca359e1a63b
Skyshader77 <anguyen2050813@gmail.com>
2024-10-21 22:00:49 -0400

    fix: fixed 404 everytime we refresh

--- server/app/index.ts
+++ server/app/index.ts
@@ -2,16 +2,17 @@ import { AppModule } from '@app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
+import * as express from 'express';
+import { join } from 'path';
 const bootstrap = async () => {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
     app.useGlobalPipes(new ValidationPipe());
     app.enableCors({
-        origin: '*', 
+        origin: '*',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
         allowedHeaders: 'Content-Type, Authorization',
-      });
+    });
     const config = new DocumentBuilder()
         .setTitle('Cadriciel Serveur')
         .setDescription('Serveur du projet de base pour le cours de LOG2990')
@@ -20,6 +21,18 @@ const bootstrap = async () => {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api/docs', app, document);
     SwaggerModule.setup('', app, document);
+
+    // Serve static files
+    app.use(express.static(join(__dirname, '..', 'client')));
+
+    // Handle client-side routing
+    app.use('*', (req, res, next) => {
+        if (req.path.startsWith('/api')) {
+            next();
+        } else {
+            res.sendFile(join(__dirname, '..', 'client', 'index.html'));
+        }
+    });
 
     await app.listen(process.env.PORT);
 };
