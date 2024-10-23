1da6903a670eb2d1452c2b7ee4416bde056606ec
Skyshader77 <anguyen2050813@gmail.com>
2024-10-23 14:09:10 -0400

    feat: added listeners for endTurn

--- client/src/app/components/game-buttons/game-buttons.component.ts
+++ client/src/app/components/game-buttons/game-buttons.component.ts
@@ -1,8 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { GameLogicSocketService } from '@app/services/communication-services/game-logic-socket.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFlag, faHand, faHandPointer, faPersonBurst, faPersonRunning } from '@fortawesome/free-solid-svg-icons';
-
 @Component({
     selector: 'app-game-buttons',
     standalone: true,
@@ -11,6 +11,10 @@ import { faFlag, faHand, faHandPointer, faPersonBurst, faPersonRunning } from '@
     styleUrl: './game-buttons.component.scss',
 })
 export class GameButtonsComponent {
+    constructor(
+        public gameLogicSocketService: GameLogicSocketService,
+    ) {}
+
     @Output() abandon = new EventEmitter<void>();
 
     handIcon = faHand;
@@ -24,5 +28,9 @@ export class GameButtonsComponent {
 
     abandonGame() {
         this.abandon.emit();
+    }
+
+    finishTurn() {
+        this.gameLogicSocketService.endTurn();
     }
 }
