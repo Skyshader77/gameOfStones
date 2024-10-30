2134fad3eae0ad6c5174076acdc68fee26be7dc7
NikolaiGTR <nikolai.olekhnovitch@polymtl.ca>
2024-10-30 15:45:24 -0400

    (feat) Added room event for available avatars

--- common/interfaces/sockets.events/room.events.ts
+++ common/interfaces/sockets.events/room.events.ts
@@ -12,4 +12,5 @@ export enum RoomEvents {
     RoomLocked = 'roomLocked',
     RoomClosed = 'roomClosed',
     PlayerLimitReached = 'playerLimitReached',
+    AvailableAvatars = 'availableAvatars'
 }
