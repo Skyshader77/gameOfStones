6f0ceaf4e63efd1ce9ad1d83b7e35aeaca049922
Thierry Champion <thierry.champion@polymtl.ca>
2024-11-01 23:04:59 -0400

    feat: fight service for fight actions

--- common/interfaces/fight.ts
+++ common/interfaces/fight.ts
@@ -2,8 +2,15 @@ import { Player } from "@app/interfaces/player";
 
 export class Fight {
     fighters: Player[];
+    winner: Player | null;
     numbEvasionsLeft: number[];
     currentFighter: string;
     hasPendingAction: boolean;
 }
 
+export interface AttackResult {
+    hasDealtDamage: boolean;
+    wasWinningBlow: boolean;
+    attackRoll: number;
+    defenseRoll: number;
+}
--- server/app/services/fight/fight/fight.service.ts
+++ server/app/services/fight/fight/fight.service.ts
@@ -3,6 +3,7 @@ import { RoomGame } from '@app/interfaces/room-game';
 import { RoomManagerService } from '@app/services/room-manager/room-manager.service';
 import { Injectable } from '@nestjs/common';
 import { EVASION_COUNT, EVASION_PROBABILITY } from './fight.service.constants';
+import { AttackResult, Fight } from '@common/interfaces/fight';
 
 @Injectable()
 export class FightService {
@@ -18,6 +19,7 @@ export class FightService {
 
         room.game.fight = {
             fighters: [currentPlayer, opponentPlayer],
+            winner: null,
             numbEvasionsLeft: [EVASION_COUNT, EVASION_COUNT],
             currentFighter:
                 currentPlayer.playerInGame.movementSpeed < opponentPlayer.playerInGame.movementSpeed
@@ -29,16 +31,49 @@ export class FightService {
         return true;
     }
 
-    attack(room: RoomGame, attackerName: string) {
+    attack(fight: Fight): AttackResult {
+        const attacker = fight.fighters.find((player) => player.playerInfo.userName === fight.currentFighter);
+        const defender = fight.fighters.find((player) => player.playerInfo.userName !== fight.currentFighter);
 
+        const attackRoll = Math.floor(Math.random() * attacker.playerInGame.dice.attackDieValue) + 1;
+        const defenseRoll = Math.floor(Math.random() * defender.playerInGame.dice.defenseDieValue) + 1;
+
+        const attackResult: AttackResult = {
+            hasDealtDamage: this.hasPlayerDealtDamage(attacker, defender, [attackRoll, defenseRoll]),
+            wasWinningBlow: false,
+            attackRoll,
+            defenseRoll,
+        };
+
+        if (attackResult.hasDealtDamage) {
+            defender.playerInGame.remainingHp--;
+            if (defender.playerInGame.remainingHp === 0) {
+                fight.winner = attacker;
+                attackResult.wasWinningBlow = true;
+            }
+        }
+
+        return attackResult;
     }
 
-    evade(room: RoomGame, evaderName: string) {
+    evade(fight: Fight): boolean {
+        const evaderIndex = fight.fighters.findIndex((player) => player.playerInfo.userName === fight.currentFighter);
 
+        let evaded = false;
+
+        if (fight.numbEvasionsLeft[evaderIndex] === 0) return evaded;
+
+        if (this.hasPlayerEvaded()) {
+            evaded = true;
+        } else {
+            fight.numbEvasionsLeft[evaderIndex]--;
+            evaded = false;
+        }
+        return evaded;
     }
 
-    private hasPlayerDealtDamage(): boolean {
-        return false;
+    private hasPlayerDealtDamage(attacker: Player, defender: Player, rolls: number[]): boolean {
+        return attacker.playerInGame.attack + rolls[0] - (defender.playerInGame.defense + rolls[1]) > 0;
     }
 
     private hasPlayerEvaded(): boolean {
