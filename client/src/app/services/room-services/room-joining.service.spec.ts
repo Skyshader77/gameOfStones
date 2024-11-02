017e7b80ab5158661e286fe5ddcd3f31d8249f38
Benjamin Perrier <ben.perrier02@gmail.com>
2024-11-02 15:20:13 -0400

    (test) room joining service tested

--- client/src/app/services/room-services/room-joining.service.spec.ts
+++ client/src/app/services/room-services/room-joining.service.spec.ts
@@ -1,16 +1,73 @@
 import { TestBed } from '@angular/core/testing';
-
-// import { RoomJoiningService } from './room-joining.service';
+import { RoomJoiningService } from './room-joining.service';
+import { RoomAPIService } from '@app/services/api-services/room-api.service';
+import { RoomSocketService } from '@app/services/communication-services/room-socket.service';
+import { RoomStateService } from './room-state.service';
+import { of } from 'rxjs';
+import { MOCK_PLAYERS } from '@app/constants/tests.constants';
 
 describe('RoomJoiningService', () => {
-    // let service: RoomJoiningService;
+    let service: RoomJoiningService;
+    let roomAPIServiceSpy: jasmine.SpyObj<RoomAPIService>;
+    let roomSocketServiceSpy: jasmine.SpyObj<RoomSocketService>;
+    let roomStateService: RoomStateService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
-        // service = TestBed.inject(RoomJoiningService);
+        roomAPIServiceSpy = jasmine.createSpyObj('RoomAPIService', ['checkRoomExists']);
+        roomSocketServiceSpy = jasmine.createSpyObj('RoomSocketService', ['requestJoinRoom', 'handlePlayerCreationOpened']);
+
+        TestBed.configureTestingModule({
+            providers: [
+                RoomJoiningService,
+                RoomStateService,
+                { provide: RoomAPIService, useValue: roomAPIServiceSpy },
+                { provide: RoomSocketService, useValue: roomSocketServiceSpy },
+            ],
+        });
+
+        service = TestBed.inject(RoomJoiningService);
+        roomStateService = TestBed.inject(RoomStateService); // Get the actual instance
+        service.playerToJoin = MOCK_PLAYERS[0];
     });
 
     it('should be created', () => {
-        // expect(service).toBeTruthy();
+        expect(service).toBeTruthy();
+    });
+
+    it('should get roomCode from RoomStateService', () => {
+        roomStateService.roomCode = '1234';
+        expect(service.roomCode).toBe('1234');
+    });
+
+    it('should set roomCode in RoomStateService', () => {
+        service.roomCode = '5678';
+        expect(roomStateService.roomCode).toBe('5678');
+    });
+
+    it('should validate input as a 4-digit number', () => {
+        expect(service.isValidInput('1234')).toBeTrue();
+        expect(service.isValidInput('abcd')).toBeFalse();
+        expect(service.isValidInput('123')).toBeFalse();
+        expect(service.isValidInput('12345')).toBeFalse();
+    });
+
+    it('should check if a room exists by calling RoomAPIService', (done) => {
+        roomAPIServiceSpy.checkRoomExists.and.returnValue(of(true));
+
+        service.doesRoomExist('1234').subscribe((exists) => {
+            expect(exists).toBeTrue();
+            expect(roomAPIServiceSpy.checkRoomExists).toHaveBeenCalledWith('1234');
+            done();
+        });
+    });
+
+    it('should request to join a room through RoomSocketService', () => {
+        service.requestJoinRoom('1234');
+        expect(roomSocketServiceSpy.requestJoinRoom).toHaveBeenCalledWith('1234', MOCK_PLAYERS[0]);
+    });
+
+    it('should notify RoomSocketService when player creation is opened', () => {
+        service.handlePlayerCreationOpened('1234');
+        expect(roomSocketServiceSpy.handlePlayerCreationOpened).toHaveBeenCalledWith('1234');
     });
 });
