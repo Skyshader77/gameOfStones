50f0c7927538a6148886202f6936a891d33bbe82
Coossy <charles-emilek7@hotmail.com>
2024-11-03 14:25:51 -0500

    feat(player-info): displays the player's information, but remains Hpmax, dice, and number of actions to complete

--- client/src/app/components/player-info/player-info.component.ts
+++ client/src/app/components/player-info/player-info.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { PlayerInfoField } from '@app/pages/play-page/play-page.component';
+import { Component } from '@angular/core';
+import { MyPlayerService } from '@app/services/room-services/my-player.service';
 
 @Component({
     selector: 'app-player-info',
@@ -10,18 +10,5 @@ import { PlayerInfoField } from '@app/pages/play-page/play-page.component';
     styleUrls: [],
 })
 export class PlayerInfoComponent {
-    @Input() playerInfoField!: PlayerInfoField; // Dans un service
-
-    get playerInfo() {
-        return [
-            { label: 'Nom', value: this.playerInfoField.name },
-            { label: 'Vie', value: `${this.playerInfoField.hp} / ${this.playerInfoField.hpMax}` },
-            { label: 'Rapidité', value: this.playerInfoField.speed },
-            { label: 'Attaque', value: this.playerInfoField.attack },
-            { label: 'Défense', value: this.playerInfoField.defense },
-            { label: 'Bonus D6', value: this.playerInfoField.d6Bonus },
-            { label: 'Points déplacement', value: this.playerInfoField.movementPoints },
-            { label: "Nombre d'actions", value: this.playerInfoField.numberOfActions },
-        ];
-    }
+    constructor(protected myPlayerService: MyPlayerService) {}
 }
--- client/src/app/services/room-services/my-player.service.ts
+++ client/src/app/services/room-services/my-player.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AvatarChoice } from '@app/constants/player.constants';
 import { Player } from '@app/interfaces/player';
 import { PlayerRole } from '@common/constants/player.constants';
 
@@ -15,4 +16,57 @@ export class MyPlayerService {
     getUserName(): string {
         return this.myPlayer?.playerInfo.userName;
     }
+
+    getAvatar(): AvatarChoice {
+        return this.myPlayer?.playerInfo.avatar;
+    }
+
+    getRemainingHp(): number {
+        return this.myPlayer?.playerInGame.hp;
+    }
+
+    /*  getMaxHp(): number {
+        return this.myPlayer?.playerInGame.hp;
+    } */
+
+    getMovementSpeed(): number {
+        return this.myPlayer?.playerInGame.movementSpeed;
+    }
+
+    getAttack(): number {
+        return this.myPlayer?.playerInGame.attack;
+    }
+
+    getDefense(): number {
+        return this.myPlayer?.playerInGame.defense;
+    }
+
+    // marche pas pour afficher attackValue et defenseValue
+    /*  getD6(): { attackValue: number; defenseValue: number } | undefined {
+        if (this.myPlayer?.playerInGame.dice === D6_ATTACK_FIELDS) {
+            return {
+                attackValue: D6_ATTACK_FIELDS.attackDieValue,
+                defenseValue: D6_ATTACK_FIELDS.defenseDieValue,
+            };
+        } else if (this.myPlayer?.playerInGame.dice === D6_DEFENCE_FIELDS) {
+            return {
+                attackValue: D6_DEFENCE_FIELDS.attackDieValue,
+                defenseValue: D6_DEFENCE_FIELDS.defenseDieValue,
+            };
+        }
+        return undefined;
+    } */
+
+    // à retirer juste pour voir que ça
+    getD6(): number {
+        return 99;
+    }
+
+    getRemainingMovement(): number {
+        return this.myPlayer?.playerInGame.remainingMovement;
+    }
+
+    /* getNumberOfActions(): number {
+        return this.myPlayer?.playerInGame.numberOfActions;
+    } */
 }
