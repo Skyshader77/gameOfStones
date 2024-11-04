7cd66e097afb899f5a76911112b3c55a65729b36
NikolaiGTR <nikolai.olekhnovitch@polymtl.ca>
2024-11-04 15:44:43 -0500

    (test) added tests for message schema

---
+++ server/app/model/schema/message.schema.spec.ts
@@ -0,0 +1,22 @@
+import { Message } from "./message.schema";
+
+describe('Message', () => {
+    it('should create a Message instance', () => {
+        const message = new Message();
+        expect(message).toBeDefined();
+    });
+
+    it('should have a title property of type string', () => {
+        const message = new Message();
+        message.title = 'Mon Message';
+        expect(typeof message.title).toBe('string');
+        expect(message.title).toBe('Mon Message');
+    });
+
+    it('should have a body property of type string', () => {
+        const message = new Message();
+        message.body = 'Je suis envoyé à partir de la documentation!';
+        expect(typeof message.body).toBe('string');
+        expect(message.body).toBe('Je suis envoyé à partir de la documentation!');
+    });
+});
