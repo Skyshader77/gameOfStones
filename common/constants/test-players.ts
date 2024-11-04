9c3d3e717653f16ce9ed5d82fee024da9ddf754c
Skyshader77 <anguyen2050813@gmail.com>
2024-11-04 09:47:35 -0500

    forgot common

--- common/constants/test-players.ts
+++ common/constants/test-players.ts
@@ -18,3 +18,21 @@ export const MOCK_PLAYER_IN_GAME: PlayerInGame = {
     remainingHp: 4,
     remainingActions: 1,
 };
+
+export const MOCK_PLAYER_IN_GAME_ABANDONNED: PlayerInGame = {
+    attributes: {
+        hp: 4,
+        speed: 6,
+        attack: 4,
+        defense: 4,
+    },
+    dice: ATTACK_DICE,
+    inventory: [],
+    currentPosition: { x: 0, y: 0 },
+    startPosition: { x: 0, y: 0 },
+    winCount: 0,
+    hasAbandoned: true,
+    remainingMovement: 6,
+    remainingHp: 4,
+    remainingActions: 1,
+};
