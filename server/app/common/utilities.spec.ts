c955eecf56b89827e2219ecf7f7a9f095e1f16c7
Skyshader77 <anguyen2050813@gmail.com>
2024-11-05 18:39:59 -0500

    fixed utilities function and added tests

--- server/app/common/utilities.spec.ts
+++ server/app/common/utilities.spec.ts
@@ -1,7 +1,7 @@
 import { MOCK_ROOM_GAMES, MOVEMENT_CONSTANTS } from '@app/constants/player.movement.test.constants';
 import { RoomGame } from '@app/interfaces/room-game';
 import { Vec2 } from '@common/interfaces/vec2';
-import { isAnotherPlayerPresentOnTile, isCoordinateWithinBoundaries } from './utilities';
+import { getNearestPositions, isAnotherPlayerPresentOnTile, isCoordinateWithinBoundaries } from './utilities';
 
 describe('isAnotherPlayerPresentOnTile', () => {
     it('should return true when another player is at x=1 and y=1', () => {
@@ -47,5 +47,28 @@ describe('isCoordinateWithinBoundaries', () => {
     it('should return true for a valid position', () => {
         const room = JSON.parse(JSON.stringify(MOCK_ROOM_GAMES.multiplePlayers)) as RoomGame;
         expect(isCoordinateWithinBoundaries({ x: 0, y: 0 }, room.game.map.mapArray)).toBe(true);
+    });
+});
+
+describe('getNearestPositions', () => {
+    it('should return 4 positions around the center for range 1', () => {
+        const position: Vec2 = { x: 0, y: 0 };
+        const range = 1;
+        const expectedResultLength = 6;
+        const result = getNearestPositions(position, range);
+        expect(result.length).toBe(expectedResultLength);
+        expect(result).toContainEqual({ x: 0, y: -1 });
+        expect(result).toContainEqual({ x: 1, y: 1 });
+        expect(result).toContainEqual({ x: -1, y: -1 });
+        expect(result).toContainEqual({ x: -1, y: 0 });
+        expect(result).toContainEqual({ x: 1, y: 0 });
+        expect(result).toContainEqual({ x: 0, y: 1 });
+    });
+
+    it('should exclude the center position (0,0) in the result', () => {
+        const position: Vec2 = { x: 0, y: 0 };
+        const range = 1;
+        const positions = getNearestPositions(position, range);
+        expect(positions).not.toContainEqual(position);
     });
 });
