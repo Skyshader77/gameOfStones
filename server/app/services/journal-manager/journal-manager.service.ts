94558bf25382bcc45447c3e8f1be7fb1c2935b1e
Thierry Champion <thierry.champion@polymtl.ca>
2024-11-04 20:04:51 -0500

    fix: smaller include in journal manager

--- server/app/services/journal-manager/journal-manager.service.ts
+++ server/app/services/journal-manager/journal-manager.service.ts
@@ -4,32 +4,7 @@ import { JournalLog } from '@common/interfaces/message';
 import { JournalEntry } from '@common/enums/journal-entry.enum';
 import { RoomGame } from '@app/interfaces/room-game';
 import { AttackResult } from '@common/interfaces/fight';
-import {
-    ABANDON_LOG,
-    AND,
-    ATTACK_DICE_LOG,
-    ATTACK_LOG,
-    BECAUSE,
-    CLOSED_DOOR_LOG,
-    COMMA,
-    DAMAGE_RESULT_LOG,
-    DEFENSE_DICE_LOG,
-    EVASION_LOG,
-    FAILED_EVASION_LOG,
-    FIGHT_NO_WINNER_LOG,
-    FIGHT_WINNER_LOG,
-    GAME_END_LOG,
-    INFLICT_DAMAGE_LOG,
-    LAST_STANDING_LOG,
-    NO_DAMAGE_LOG,
-    NO_DAMAGE_RESULT_LOG,
-    OPEN_DOOR_LOG,
-    START_FIGHT_LOG,
-    SUCCESS_EVASION_LOG,
-    THEN,
-    TURN_START_LOG,
-    WINNER_LOG,
-} from '@app/constants/journal.constants';
+import * as constants from '@app/constants/journal.constants';
 
 @Injectable()
 export class JournalManagerService {
@@ -66,7 +41,7 @@ export class JournalManagerService {
 
     fightAttackResultJournal(room: RoomGame, attackResult: AttackResult): JournalLog {
         const fight = room.game.fight;
-        const rolls = ATTACK_DICE_LOG + attackResult.attackRoll + DEFENSE_DICE_LOG + attackResult.defenseRoll;
+        const rolls = constants.ATTACK_DICE_LOG + attackResult.attackRoll + constants.DEFENSE_DICE_LOG + attackResult.defenseRoll;
         const calculation =
             fight.fighters[fight.currentFighter].playerInGame.attributes.attack +
             ' + ' +
@@ -76,15 +51,15 @@ export class JournalManagerService {
             ' + ' +
             attackResult.defenseRoll +
             ')' +
-            (attackResult.hasDealtDamage ? DAMAGE_RESULT_LOG : NO_DAMAGE_RESULT_LOG);
+            (attackResult.hasDealtDamage ? constants.DAMAGE_RESULT_LOG : constants.NO_DAMAGE_RESULT_LOG);
         const conclusion =
             fight.fighters[fight.currentFighter].playerInfo.userName +
-            (attackResult.hasDealtDamage ? INFLICT_DAMAGE_LOG : NO_DAMAGE_LOG) +
+            (attackResult.hasDealtDamage ? constants.INFLICT_DAMAGE_LOG : constants.NO_DAMAGE_LOG) +
             fight.fighters[(fight.currentFighter + 1) % 2].playerInfo.userName;
 
         return {
             message: {
-                content: rolls + BECAUSE + calculation + THEN + conclusion,
+                content: rolls + constants.BECAUSE + calculation + constants.THEN + conclusion,
                 time: new Date(),
             },
             entry: JournalEntry.FightAttackResult,
@@ -96,7 +71,7 @@ export class JournalManagerService {
         const fight = room.game.fight;
         const content =
             fight.fighters[fight.currentFighter].playerInfo.userName +
-            (evasionSuccessful ? SUCCESS_EVASION_LOG : FAILED_EVASION_LOG + fight.numbEvasionsLeft[fight.currentFighter]);
+            (evasionSuccessful ? constants.SUCCESS_EVASION_LOG : constants.FAILED_EVASION_LOG + fight.numbEvasionsLeft[fight.currentFighter]);
         return {
             message: {
                 content,
@@ -110,7 +85,7 @@ export class JournalManagerService {
     abandonJournal(deserterName: string): JournalLog {
         return {
             message: {
-                content: deserterName + ABANDON_LOG,
+                content: deserterName + constants.ABANDON_LOG,
                 time: new Date(),
             },
             entry: JournalEntry.PlayerAbandon,
@@ -121,7 +96,7 @@ export class JournalManagerService {
     private turnStartJournal(room: RoomGame): JournalLog {
         return {
             message: {
-                content: TURN_START_LOG + room.game.currentPlayer,
+                content: constants.TURN_START_LOG + room.game.currentPlayer,
                 time: new Date(),
             },
             entry: JournalEntry.TurnStart,
@@ -132,7 +107,7 @@ export class JournalManagerService {
     private doorOpenedJournal(room: RoomGame): JournalLog {
         return {
             message: {
-                content: room.game.currentPlayer + OPEN_DOOR_LOG,
+                content: room.game.currentPlayer + constants.OPEN_DOOR_LOG,
                 time: new Date(),
             },
             entry: JournalEntry.DoorOpen,
@@ -143,7 +118,7 @@ export class JournalManagerService {
     private doorClosedJournal(room: RoomGame): JournalLog {
         return {
             message: {
-                content: room.game.currentPlayer + CLOSED_DOOR_LOG,
+                content: room.game.currentPlayer + constants.CLOSED_DOOR_LOG,
                 time: new Date(),
             },
             entry: JournalEntry.DoorClose,
@@ -155,7 +130,7 @@ export class JournalManagerService {
         const opponent = room.game.fight.fighters.find((fighter) => fighter.playerInfo.userName !== room.game.currentPlayer);
         return {
             message: {
-                content: room.game.currentPlayer + START_FIGHT_LOG + opponent.playerInfo.userName,
+                content: room.game.currentPlayer + constants.START_FIGHT_LOG + opponent.playerInfo.userName,
                 time: new Date(),
             },
             entry: JournalEntry.FightStart,
@@ -169,7 +144,7 @@ export class JournalManagerService {
             message: {
                 content:
                     fight.fighters[fight.currentFighter].playerInfo.userName +
-                    ATTACK_LOG +
+                    constants.ATTACK_LOG +
                     fight.fighters[(fight.currentFighter + 1) % 2].playerInfo.userName,
                 time: new Date(),
             },
@@ -182,7 +157,7 @@ export class JournalManagerService {
         const fight = room.game.fight;
         return {
             message: {
-                content: fight.fighters[fight.currentFighter].playerInfo.userName + EVASION_LOG,
+                content: fight.fighters[fight.currentFighter].playerInfo.userName + constants.EVASION_LOG,
                 time: new Date(),
             },
             entry: JournalEntry.FightAttack,
@@ -193,8 +168,8 @@ export class JournalManagerService {
     private fightEndJournal(room: RoomGame): JournalLog {
         const fight = room.game.fight;
         const content = fight.result.winner
-            ? fight.result.winner + FIGHT_WINNER_LOG + fight.result.loser
-            : fight.fighters[0].playerInfo.userName + AND + fight.fighters[1].playerInfo.userName + FIGHT_NO_WINNER_LOG;
+            ? fight.result.winner + constants.FIGHT_WINNER_LOG + fight.result.loser
+            : fight.fighters[0].playerInfo.userName + constants.AND + fight.fighters[1].playerInfo.userName + constants.FIGHT_NO_WINNER_LOG;
         return {
             message: {
                 content,
@@ -208,7 +183,7 @@ export class JournalManagerService {
     private playerWinJournal(room: RoomGame): JournalLog {
         return {
             message: {
-                content: room.game.winner + WINNER_LOG,
+                content: room.game.winner + constants.WINNER_LOG,
                 time: new Date(),
             },
             entry: JournalEntry.PlayerWin,
@@ -223,15 +198,15 @@ export class JournalManagerService {
             if (!player.playerInGame.hasAbandoned) {
                 content += player.playerInfo.userName;
                 if (index === remainingPlayers.length - 2) {
-                    content += AND;
+                    content += constants.AND;
                 } else if (index < remainingPlayers.length - 1) {
-                    content += COMMA;
+                    content += constants.COMMA;
                 }
             }
         });
         return {
             message: {
-                content: content + (remainingPlayers.length > 1 ? GAME_END_LOG : LAST_STANDING_LOG),
+                content: content + (remainingPlayers.length > 1 ? constants.GAME_END_LOG : constants.LAST_STANDING_LOG),
                 time: new Date(),
             },
             entry: JournalEntry.PlayerWin,
