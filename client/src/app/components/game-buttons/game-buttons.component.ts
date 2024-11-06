34f9c4c2fb7888950ee50f7d8c5627f453987f7b
Benjamin Perrier <ben.perrier02@gmail.com>
2024-11-05 22:27:54 -0500

    tests

--- client/src/app/components/game-buttons/game-buttons.component.ts
+++ client/src/app/components/game-buttons/game-buttons.component.ts
@@ -3,10 +3,10 @@ import { RouterLink } from '@angular/router';
 import { BUTTONS_ICONS } from '@app/constants/game-buttons.constants';
 import { GameLogicSocketService } from '@app/services/communication-services/game-logic-socket.service';
 import { PlayButtonsService } from '@app/services/play-buttons/play-buttons.service';
+import { RenderingStateService } from '@app/services/rendering-services/rendering-state.service';
 import { FightStateService } from '@app/services/room-services/fight-state.service';
 import { MyPlayerService } from '@app/services/room-services/my-player.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { RenderingStateService } from '@app/services/rendering-services/rendering-state.service';
 
 @Component({
     selector: 'app-game-buttons',
