ddef7b1a5e882e90866cdd6cbbeb4b6dad21ca09
Thierry Champion <thierry.champion@polymtl.ca>
2024-11-07 15:17:56 -0500

    feat: end statistics interface

---
+++ common/interfaces/end-statistics.ts
@@ -0,0 +1,20 @@
+export interface GameEndStats {
+    timeTaken: Date;
+    turnCount: number;
+    percentageDoorsUsed: number;
+    percentageTilesTraversed: number;
+    numberOfPlayersWithFlag: number;
+    playerStats: PlayerEndStats[];
+}
+
+export interface PlayerEndStats {
+    name: string;
+    fightCount: number;
+    winCount: number;
+    lossCount: number;
+    evasionCount: number;
+    totalHpLost: number;
+    totalDamageDealt: number;
+    itemCount: number;
+    percentageTilesTraversed: number;
+}
