0781efc94e9159d7363fff5e9ebddc7aa9a6d157
Coossy <charles-emilek7@hotmail.com>
2024-11-11 19:26:37 -0500

    fix: lint tests pass

--- client/src/app/services/debug-mode/debug-mode.service.spec.ts
+++ client/src/app/services/debug-mode/debug-mode.service.spec.ts
@@ -1,16 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
 import { DebugModeService } from './debug-mode.service';
 
 describe('DebugModeService', () => {
-  let service: DebugModeService;
+    let service: DebugModeService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DebugModeService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(DebugModeService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
