e068a13deae221e6c653e4aa07bfbe5cd0cbd7fd
Thierry Champion <thierry.champion@polymtl.ca>
2024-11-24 19:33:26 -0500

    fix: comment in MR

--- client/src/app/components/player-list/player-list.component.ts
+++ client/src/app/components/player-list/player-list.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AVATAR_PROFILE } from '@app/constants/player.constants';
+import { Player } from '@app/interfaces/player';
 import { MyPlayerService } from '@app/services/room-services/my-player.service';
 import { PlayerListService } from '@app/services/room-services/player-list.service';
 import { PlayerRole } from '@common/enums/player-role.enum';
@@ -17,9 +18,21 @@ export class PlayerListComponent implements OnInit, OnDestroy {
     playerRole = PlayerRole;
 
     constructor(
-        protected playerListService: PlayerListService,
-        public myPlayerService: MyPlayerService,
+        private playerListService: PlayerListService,
+        private myPlayerService: MyPlayerService,
     ) {}
+
+    get playerList() {
+        return this.playerListService.playerList;
+    }
+
+    isMyName(playerName: string): boolean {
+        return this.myPlayerService.getUserName() === playerName;
+    }
+
+    showKick(player: Player): boolean {
+        return player.playerInfo.role !== PlayerRole.Organizer && this.myPlayerService.isOrganizer();
+    }
 
     ngOnInit(): void {
         this.playerListService.initialize();
