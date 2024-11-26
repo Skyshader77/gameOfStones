c8c981babe8946c9aa36df83bbe8d6f42a061dd1
Nabil Ibarissen <nabilibarissen2003@gmail.com>
2024-11-25 19:07:42 -0500

    feat: animation on usable items

--- client/src/app/components/inventory/inventory.component.ts
+++ client/src/app/components/inventory/inventory.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ITEM_PATHS } from '@app/constants/conversion.constants';
+import { RenderingStateService } from '@app/services/rendering-services/rendering-state.service';
 import { MyPlayerService } from '@app/services/room-services/my-player.service';
 import { ItemType } from '@common/enums/item-type.enum';
 
@@ -8,12 +9,24 @@ import { ItemType } from '@common/enums/item-type.enum';
     standalone: true,
     imports: [],
     templateUrl: './inventory.component.html',
+    styleUrls: ['./inventory.component.css'],
 })
 export class InventoryComponent {
-    constructor(private myPlayerService: MyPlayerService) {}
+    constructor(
+        private myPlayerService: MyPlayerService,
+        private rendererStateService: RenderingStateService,
+    ) {}
 
     get myInventory() {
         return this.myPlayerService.getInventory();
+    }
+
+    get canUseItems() {
+        return this.rendererStateService.displayActions;
+    }
+
+    isSpecialItem(item: ItemType): boolean {
+        return item === ItemType.GraniteHammer || item === ItemType.GeodeBomb;
     }
 
     getItemImagePaths(items: ItemType[]): string[] {
