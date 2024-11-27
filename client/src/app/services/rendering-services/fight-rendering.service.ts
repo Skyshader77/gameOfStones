1c2482b92016faf17e48a2d778914d51b59366b6
Skyshader77 <anguyen2050813@gmail.com>
2024-11-26 19:23:39 -0500

    fix: bug with not being able to end fight with AI player

--- client/src/app/services/rendering-services/fight-rendering.service.ts
+++ client/src/app/services/rendering-services/fight-rendering.service.ts
@@ -31,9 +31,9 @@ import { FightStateService } from '@app/services/room-services/fight-state.servi
 import { MyPlayerService } from '@app/services/room-services/my-player.service';
 import { Avatar } from '@common/enums/avatar.enum';
 import { Player } from '@common/interfaces/player';
+import { Vec2 } from '@common/interfaces/vec2';
 import { RenderingStateService } from './rendering-state.service';
 import { SpriteService } from './sprite.service';
-import { Vec2 } from '@common/interfaces/vec2';
 
 @Injectable({
     providedIn: 'root',
@@ -89,7 +89,7 @@ export class FightRenderingService {
         if (this.opponentStartingPosition.x >= MY_STARTING_POSITION_Y || this.myStartingPosition.x <= OPPONENT_STARTING_POSITION_Y) {
             this.fightStateService.fightState = FightState.Idle;
             this.resetPositions();
-            if (this.myPlayerService.isCurrentFighter) {
+            if (this.myPlayerService.isCurrentFighter || this.fightStateService.isAIInFight()) {
                 this.fightSocketService.sendDesiredFightTimer();
             }
             this.blackOpacity = END_BLACK_OPACITY;
@@ -154,7 +154,7 @@ export class FightRenderingService {
             this.isAttackingFoward = !this.isAttackingFoward;
             this.fightStateService.fightState = FightState.Idle;
             this.resetPositions();
-            if (this.myPlayerService.isCurrentFighter) {
+            if (this.myPlayerService.isCurrentFighter|| this.fightStateService.isAIInFight()) {
                 this.fightSocketService.endFightAction();
             }
         }
@@ -186,7 +186,7 @@ export class FightRenderingService {
     }
 
     renderEvade() {
-        if (this.myPlayerService.isCurrentFighter) {
+        if (this.myPlayerService.isCurrentFighter || this.fightStateService.isAIInFight()) {
             this.myStartingPosition.x -= PIXEL_MOVEMENT;
         } else {
             this.opponentStartingPosition.x += PIXEL_MOVEMENT;
@@ -198,7 +198,7 @@ export class FightRenderingService {
         if (this.blackOpacity >= 1) {
             this.fightStateService.fightState = FightState.Idle;
             this.rendererState.fightStarted = false;
-            if (this.myPlayerService.isCurrentFighter) {
+            if (this.myPlayerService.isCurrentFighter || this.fightStateService.isAIInFight()) {
                 this.fightSocketService.endFightAction();
             }
         }
