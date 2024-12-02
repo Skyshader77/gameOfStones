83156d1a714bc7cc59424924b44bf822214c4860
Coossy <charles-emilek7@hotmail.com>
2024-12-02 16:45:26 -0500

    test: player-list.service tested

--- client/src/app/services/states/player-list/player-list.service.spec.ts
+++ client/src/app/services/states/player-list/player-list.service.spec.ts
@@ -1,16 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { ROOM_CLOSED_MESSAGE } from '@app/constants/init-page-redirection.constants';
-import { MOCK_PLAYER_STARTS_TESTS, MOCK_PLAYERS } from '@app/constants/tests.constants';
+import { MOCK_PLAYER_INFO, MOCK_PLAYER_RENDER_INFO, MOCK_PLAYER_STARTS_TESTS, MOCK_PLAYERS } from '@app/constants/tests.constants';
 import { Player } from '@app/interfaces/player';
 import { AudioService } from '@app/services/audio/audio.service';
 import { SocketService } from '@app/services/communication-services/socket/socket.service';
+import { PlayerCreationService } from '@app/services/player-creation-services/player-creation.service';
 import { MyPlayerService } from '@app/services/states/my-player/my-player.service';
 import { ModalMessageService } from '@app/services/utilitary/modal-message/modal-message.service';
+import { MOCK_PLAYER_IN_GAME } from '@common/constants/test-players';
 import { Gateway } from '@common/enums/gateway.enum';
+import { ItemType } from '@common/enums/item-type.enum';
+import { PlayerRole } from '@common/enums/player-role.enum';
 import { GameEvents } from '@common/enums/sockets-events/game.events';
 import { RoomEvents } from '@common/enums/sockets-events/room.events';
 import { DeadPlayerPayload } from '@common/interfaces/player';
+import { Vec2 } from '@common/interfaces/vec2';
 import { Observable, of } from 'rxjs';
 import { PlayerListService } from './player-list.service';
 
@@ -21,6 +26,7 @@ describe('PlayerListService', () => {
     let routerSpy: jasmine.SpyObj<Router>;
     let modalMessageServiceSpy: jasmine.SpyObj<ModalMessageService>;
     let audioSpy: jasmine.SpyObj<AudioService>;
+    let playerCreationService: jasmine.SpyObj<PlayerCreationService>;
 
     beforeEach(() => {
         socketServiceSpy = jasmine.createSpyObj('SocketService', ['on', 'emit']);
@@ -29,6 +35,7 @@ describe('PlayerListService', () => {
         myPlayerServiceSpy = jasmine.createSpyObj('MyPlayerService', ['getUserName'], { isCurrentPlayer: false });
         modalMessageServiceSpy = jasmine.createSpyObj('ModalMessageService', ['setMessage']);
         audioSpy = jasmine.createSpyObj('AudioService', ['playSfx']);
+        playerCreationService = jasmine.createSpyObj('PlayerCreationService', ['createInitialRenderInfo']);
         TestBed.configureTestingModule({
             providers: [
                 PlayerListService,
@@ -37,6 +44,7 @@ describe('PlayerListService', () => {
                 { provide: Router, useValue: routerSpy },
                 { provide: ModalMessageService, useValue: modalMessageServiceSpy },
                 { provide: AudioService, useValue: audioSpy },
+                { provide: PlayerCreationService, useValue: playerCreationService },
             ],
         });
 
@@ -50,6 +58,153 @@ describe('PlayerListService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should find the correct player based on teleportInfo.playerName', () => {
+        const mockPlayerList = [
+            {
+                playerInfo: MOCK_PLAYER_INFO[0],
+                playerInGame: { ...MOCK_PLAYER_IN_GAME },
+                renderInfo: MOCK_PLAYER_RENDER_INFO,
+            } as Player,
+            {
+                playerInfo: MOCK_PLAYER_INFO[1],
+                playerInGame: { ...MOCK_PLAYER_IN_GAME },
+                renderInfo: MOCK_PLAYER_RENDER_INFO,
+            } as Player,
+        ];
+        service.playerList = [...mockPlayerList];
+        const mockTeleportInfo = {
+            playerName: 'Player 2',
+            destination: { x: 10, y: 10 },
+        };
+        const teleportObservable = of(mockTeleportInfo);
+        socketServiceSpy.on.and.returnValue(teleportObservable);
+        service['listenPlayerTeleport']();
+        const updatedPlayer = service.playerList.find((player) => player.playerInfo.userName === mockTeleportInfo.playerName);
+        expect(updatedPlayer).toBeDefined();
+        expect(updatedPlayer!.playerInGame.currentPosition).toEqual(mockTeleportInfo.destination);
+    });
+
+    it('should set player.renderInfo when the player role is AI', () => {
+        playerCreationService.createInitialRenderInfo.and.returnValue(MOCK_PLAYER_RENDER_INFO);
+        const mockPlayer = {
+            playerInfo: { role: PlayerRole.AggressiveAI },
+            renderInfo: null,
+        } as unknown as Player;
+        socketServiceSpy.on.and.callFake(<T>(gateway: Gateway, event: string): Observable<T> => {
+            if (gateway === Gateway.Room && event === RoomEvents.AddPlayer) {
+                return of(mockPlayer as unknown as string as T);
+            }
+            return of();
+        });
+        (service as any).listenPlayerAdded();
+        expect(mockPlayer.renderInfo).toEqual(MOCK_PLAYER_RENDER_INFO);
+        expect(playerCreationService.createInitialRenderInfo).toHaveBeenCalled();
+    });
+
+    it('should not set player.renderInfo when the player role is not AI', () => {
+        const mockPlayer = {
+            playerInfo: { role: PlayerRole.Human },
+            renderInfo: null,
+        } as unknown as Player;
+        socketServiceSpy.on.and.callFake(<T>(gateway: Gateway, event: string): Observable<T> => {
+            if (gateway === Gateway.Room && event === RoomEvents.AddPlayer) {
+                return of(mockPlayer as unknown as string as T);
+            }
+            return of();
+        });
+        (service as any).listenPlayerAdded();
+        expect(mockPlayer.renderInfo).toBeNull();
+        expect(playerCreationService.createInitialRenderInfo).not.toHaveBeenCalled();
+    });
+
+    it('should return true if a player is on the given tile', () => {
+        const mockPlayerList: Player[] = [
+            {
+                playerInGame: {
+                    currentPosition: { x: 2, y: 3 },
+                },
+            } as Player,
+            {
+                playerInGame: {
+                    currentPosition: { x: 5, y: 6 },
+                },
+            } as Player,
+        ];
+        service.playerList = mockPlayerList;
+        const tileToCheck: Vec2 = { x: 2, y: 3 };
+        expect(service.isPlayerOnTile(tileToCheck)).toBeTrue();
+    });
+
+    it('should return false if no player is on the given tile', () => {
+        const mockPlayerList: Player[] = [
+            {
+                playerInGame: {
+                    currentPosition: { x: 2, y: 3 },
+                },
+            } as Player,
+            {
+                playerInGame: {
+                    currentPosition: { x: 5, y: 6 },
+                },
+            } as Player,
+        ];
+        service.playerList = mockPlayerList;
+        const tileToCheck: Vec2 = { x: 8, y: 9 };
+        expect(service.isPlayerOnTile(tileToCheck)).toBeFalse();
+    });
+
+    it('should return true if current player is AI with AggressiveAI role', () => {
+        spyOn(service, 'getCurrentPlayer').and.returnValue({
+            playerInfo: MOCK_PLAYER_INFO[1],
+            playerInGame: { ...MOCK_PLAYER_IN_GAME, inventory: [ItemType.Flag] },
+            renderInfo: MOCK_PLAYER_RENDER_INFO,
+        });
+        expect(service.isCurrentPlayerAI()).toBeTrue();
+    });
+
+    it('should return false if current player is not AI (Human role)', () => {
+        spyOn(service, 'getCurrentPlayer').and.returnValue({
+            playerInfo: MOCK_PLAYER_INFO[2],
+            playerInGame: { ...MOCK_PLAYER_IN_GAME, inventory: [ItemType.Flag] },
+            renderInfo: MOCK_PLAYER_RENDER_INFO,
+        });
+        expect(service.isCurrentPlayerAI()).toBeFalse();
+    });
+
+    it('should return false if current player is not AI (Organizer role)', () => {
+        spyOn(service, 'getCurrentPlayer').and.returnValue({
+            playerInfo: MOCK_PLAYER_INFO[0],
+            playerInGame: { ...MOCK_PLAYER_IN_GAME, inventory: [ItemType.Flag] },
+            renderInfo: MOCK_PLAYER_RENDER_INFO,
+        });
+        expect(service.isCurrentPlayerAI()).toBeFalse();
+    });
+
+    it('should return true if the player has a flag in their inventory', () => {
+        const player: Player = {
+            playerInfo: MOCK_PLAYER_INFO[0],
+            playerInGame: { ...MOCK_PLAYER_IN_GAME, inventory: [ItemType.Flag] },
+            renderInfo: MOCK_PLAYER_RENDER_INFO,
+        };
+        const result = service.hasFlag(player);
+        expect(result).toBeTrue();
+    });
+
+    it('should return false if the player does not have a flag in their inventory', () => {
+        const player: Player = {
+            playerInfo: MOCK_PLAYER_INFO[1],
+            playerInGame: { ...MOCK_PLAYER_IN_GAME, inventory: [] },
+            renderInfo: MOCK_PLAYER_RENDER_INFO,
+        };
+        const result = service.hasFlag(player);
+        expect(result).toBeFalse();
+    });
+
+    it('should return undefined when the player does not exist', () => {
+        const player = service.getPlayerByName('player3');
+        expect(player).toBeUndefined();
     });
 
     it('should emit DesireKickPlayer event when removePlayer is called', () => {
@@ -151,7 +306,6 @@ describe('PlayerListService', () => {
     it('should return 0 if playerList is null or undefined', () => {
         service.playerList = null as unknown as Player[];
         expect(service.getPlayerListCount()).toBe(0);
-
         service.playerList = undefined as unknown as Player[];
         expect(service.getPlayerListCount()).toBe(0);
     });
@@ -159,10 +313,8 @@ describe('PlayerListService', () => {
     it('should return the count of players who have not abandoned', () => {
         service.playerList = [{ playerInGame: { hasAbandoned: false } } as Player, { playerInGame: { hasAbandoned: false } } as Player];
         expect(service.getPlayerListCount()).toBe(2);
-
         service.playerList = [{ playerInGame: { hasAbandoned: false } } as Player, { playerInGame: { hasAbandoned: true } } as Player];
         expect(service.getPlayerListCount()).toBe(1);
-
         service.playerList = [{ playerInGame: { hasAbandoned: true } } as Player, { playerInGame: { hasAbandoned: true } } as Player];
         expect(service.getPlayerListCount()).toBe(0);
     });
@@ -207,7 +359,6 @@ describe('PlayerListService', () => {
     it('should update playerList with correct start and current positions for players', () => {
         service.playerList = [JSON.parse(JSON.stringify(MOCK_PLAYERS[0])), JSON.parse(JSON.stringify(MOCK_PLAYERS[1]))];
         service.preparePlayersForGameStart(MOCK_PLAYER_STARTS_TESTS);
-
         expect(service.playerList[0].playerInGame.startPosition).toEqual({ x: 1, y: 1 });
         expect(service.playerList[0].playerInGame.currentPosition).toEqual({ x: 1, y: 1 });
         expect(service.playerList[1].playerInGame.startPosition).toEqual({ x: 6, y: 6 });
@@ -217,11 +368,8 @@ describe('PlayerListService', () => {
     it('should set myPlayerService.myPlayer when the player matches the current player', () => {
         const currentUserName = MOCK_PLAYERS[0].playerInfo.userName;
         myPlayerServiceSpy.getUserName.and.returnValue(currentUserName);
-
         service.playerList = [JSON.parse(JSON.stringify(MOCK_PLAYERS[0])), JSON.parse(JSON.stringify(MOCK_PLAYERS[1]))];
-
         service.preparePlayersForGameStart(MOCK_PLAYER_STARTS_TESTS);
-
         expect(myPlayerServiceSpy.myPlayer).toEqual(service.playerList[0]);
     });
 
@@ -236,14 +384,12 @@ describe('PlayerListService', () => {
                 respawnPosition: { x: 5, y: 5 },
             },
         ];
-
         service.playerList = [
             { playerInfo: { userName: 'Player1' }, playerInGame: { currentPosition: { x: 0, y: 0 } } } as Player,
             { playerInfo: { userName: 'Player2' }, playerInGame: { currentPosition: { x: 0, y: 0 } } } as Player,
         ];
 
         service.handleDeadPlayers(deadPlayers);
-
         expect(service.playerList[0].playerInGame.currentPosition).toEqual({ x: 3, y: 3 });
         expect(service.playerList[1].playerInGame.currentPosition).toEqual({ x: 5, y: 5 });
     });
@@ -255,10 +401,8 @@ describe('PlayerListService', () => {
         ];
 
         service.playerList = [...initialPlayerList];
-
         service.handleDeadPlayers([]);
         expect(service.playerList).toEqual(initialPlayerList);
-
         service.handleDeadPlayers(null as unknown as DeadPlayerPayload[]);
         expect(service.playerList).toEqual(initialPlayerList);
     });
