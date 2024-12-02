cfc66bb25d548c50539d6b57a31701b8033ce7fc
Thierry Champion <thierry.champion@polymtl.ca>
2024-12-02 17:11:58 -0500

    feat: overlord easter egg

--- client/src/app/services/communication-services/fight-socket/fight-socket.service.ts
+++ client/src/app/services/communication-services/fight-socket/fight-socket.service.ts
@@ -123,12 +123,7 @@ export class FightSocketService {
         return this.socketService.on<FightResult>(Gateway.Fight, GameEvents.FightEnd).subscribe((result) => {
             const isAIInFight = this.fightStateService.isAIInFight();
             this.fightStateService.processEndFight(result);
-            console.log(result);
-            if (result.winner === OVERLORD && this.myPlayerService.isFighting) {
-                this.audioService.playRandomSfx([Sfx.OverlordWin1, Sfx.OverlordWin2]);
-            } else if (result.loser === OVERLORD && this.myPlayerService.isFighting) {
-                this.audioService.playRandomSfx([Sfx.OverlordLose1, Sfx.OverlordLose2]);
-            }
+            this.overlordMessage(result);
             this.myPlayerService.isCurrentFighter = false;
             this.myPlayerService.isFighting = false;
             this.renderStateService.fightStarted = false;
@@ -136,5 +131,13 @@ export class FightSocketService {
                 this.gameLogicSocketService.endAction();
             }
         });
+    }
+
+    private overlordMessage(result: FightResult) {
+        if (result.winner === OVERLORD && this.myPlayerService.isFighting && !this.myPlayerService.isCurrentFighter) {
+            this.audioService.playRandomSfx([Sfx.OverlordWin1, Sfx.OverlordWin2]);
+        } else if (result.loser === OVERLORD && this.myPlayerService.isFighting && this.myPlayerService.isCurrentFighter) {
+            this.audioService.playRandomSfx([Sfx.OverlordLose1, Sfx.OverlordLose2]);
+        }
     }
 }
